e3e71efc8144cc20774e3e18a5aea1c0c5b095e2
iamtanki <iamtanki@gmail.com>
2019-04-17 17:58:02 +0800

    add

--- a.cpp
+++ a.cpp
@@ -8,23 +8,71 @@ using namespace std;
 void reverseArray(int *array, int size);
 void printArray(int *array, int size);
 
-int main(){
 
+class Box
+{
+   private:
+      double length;         // 长度
+      double breadth;        // 宽度
+      double height;         // 高度
 
-  int array[] ={1,2,3,4,5,6,7,8,9};
-
-  int size = 9 ;
-
-  printArray(array,size);
-
-  reverseArray(array, size);
-
-  cout << "the reserved array is :" << endl;
-
-  printArray(array, size);
-
-  return 0;
+   public:
+      // 成员函数声明
+      double getVolume(void);
+      void setLength( double len );
+      void setBreadth( double bre );
+      void setHeight( double hei );
+};
+ 
+// 成员函数定义
+double Box::getVolume(void)
+{
+    return length * breadth * height;
 }
+ 
+void Box::setLength( double len )
+{
+    length = len;
+}
+ 
+void Box::setBreadth( double bre )
+{
+    breadth = bre;
+}
+ 
+void Box::setHeight( double hei )
+{
+    height = hei;
+}
+ 
+// 程序的主函数
+int main()
+{
+   Box Box1;                // 声明 Box1，类型为 Box
+   Box Box2;                // 声明 Box2，类型为 Box
+   double volume = 0.0;     // 用于存储体积
+ 
+   // box 1 详述
+   Box1.setLength(6.0); 
+   Box1.setBreadth(7.0); 
+   Box1.setHeight(5.0);
+ 
+   // box 2 详述
+   Box2.setLength(12.0); 
+   Box2.setBreadth(13.0); 
+   Box2.setHeight(10.0);
+ 
+   // box 1 的体积
+   volume = Box1.getVolume();
+   cout << "Box1 的体积：" << volume <<endl;
+ 
+   // box 2 的体积
+   volume = Box2.getVolume();
+   cout << "Box2 的体积：" << volume <<endl;
+   return 0;
+}
+
+
 
 void reverseArray(int *array, int size){
 
