eaa512bfbb83a1012c6859a6b26f30e26480ba8e
iamtanki <iamtanki@gmail.com>
2019-04-23 17:36:24 +0800

    add

---
+++ b.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+struct MyException : public exception
+{
+  const char * what () 
+  {
+    return "C++ Exception";
+  }
+};
+
+
+int main(){
+
+  char data[100] ;
+
+  const char *file  = "file.txt";
+  
+  //  ofstream outfile;
+  // outfile.open(file, ios::app);
+
+  // cin.getline(data,100);
+
+  //outfile << data << endl;
+
+  // outfile.close();
+
+  //ifstream ifile;
+  // ifile.open(file);
+
+  // ifile >> data;
+
+  // cout << data << endl;
+
+  try
+  {
+    throw MyException();
+  }
+  catch(MyException& e)
+  {
+    std::cout << "MyException caught 1 " << std::endl;
+    std::cout << e.what() << std::endl;
+  }
+  catch(std::exception& e)
+  {
+    //其他的错误
+  }
+  
+  
+  return 0;
+
+}
